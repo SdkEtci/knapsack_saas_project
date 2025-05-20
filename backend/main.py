43157a39f6506473e516eb6c5b9b77e90fba3ab5
SdkEtci <126989483+SdkEtci@users.noreply.github.com>
2025-05-20 13:47:17 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_folder='frontend', static_url_path='/index.html')
+app = Flask(__name__, static_folder='../frontend', static_url_path='/index.html')
 CORS(app)
 
 def parse_array_string(s):
