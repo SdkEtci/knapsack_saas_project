30633a5f2ab1dc9312af833cd65f0c76ac5ef801
SdkEtci <126989483+SdkEtci@users.noreply.github.com>
2025-05-20 15:09:40 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -112,7 +112,11 @@ def get_knapsack_results():
     }
     
     return jsonify(response_data)
-
+    
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'index.html')
+    
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
